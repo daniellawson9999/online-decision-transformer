dbe053bd3f53ebcd97fce0b56d09deb240cb6bd9
Daniel Lawson <daniellawson9999@gmail.com>
2022-07-13 15:47:49 -0400

    cleaned

--- d4rl/d4rl/gym_minigrid/envs/__init__.py
+++
@@ -1,2 +0,0 @@
-from d4rl.gym_minigrid.envs.fourrooms import *
-from d4rl.gym_minigrid.envs.empty import *
--- d4rl/scripts/reference_scores/generate_ref_min_score.sh
+++
@@ -1,5 +0,0 @@
-for e in $(cat scripts/reference_scores/envs.txt)
-do
-    python scripts/reference_scores/generate_ref_min_score.py --env_name=$e
-done
-
